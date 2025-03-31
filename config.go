722fd1a6bae06c6bdf73f4c016b633b6911917bd
fajarstyuu <rizkyfsatriya@gmail.com>
2025-03-31 19:11:54 +0700

    Add mysql database config

---
+++ config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func Connect() {
+	var Cfg = mysql.Config{
+		User:   os.Getenv("DB_USER"),
+		Passwd: os.Getenv("DB_PASSWORD"),
+		Net:    "tcp",
+		Addr:   os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT"),
+		DBName: os.Getenv("DB_NAME"),
+	}
+
+	var err error
+	db, err = sql.Open("mysql", Cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+}
